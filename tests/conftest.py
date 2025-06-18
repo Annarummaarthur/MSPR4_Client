e79a5bb6ca5621f79784d8fe712c1ceead125b13
Beyou <159903460+Beyou-Pro@users.noreply.github.com>
2025-06-18 16:48:33 +0200

    [FIX] fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,18 @@
-# tests/conftest.py
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.db import get_db
-from app.models import Base  # Your declarative base
+from app.models import Base
 from app.main import app
 
 TEST_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
 
 Base.metadata.create_all(bind=engine)
 
