5c36974ae50223c784972b1c2d82bf33b75e5be9
Beyou <159903460+Beyou-Pro@users.noreply.github.com>
2025-07-20 18:40:33 +0200

    [DEL] useless setting in db.py

--- app/db.py
+++ app/db.py
@@ -11,7 +11,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {},
 )
 SessionLocal = sessionmaker(bind=engine)
 
